7fdcb58d67715263bda23cb84f15ef23ad37f34e
pyszczekk <karolina.pieszczekk@gmail.com>
2020-06-02 16:10:48 +0200

    move command

--- Logo/src/LogoMoveCommand.java
+++ Logo/src/LogoMoveCommand.java
@@ -9,8 +9,50 @@ public class LogoMoveCommand implements LogoCommandInterface {
     @Override
     public TurtleSituation calculateSituation(TurtleSituation currentSituation) {
         modifiedSituation = new TurtleSituation(currentSituation);
-        modifiedSituation.position.x = 0;
-        modifiedSituation.position.y = 0;
+        int x ;
+        int y;
+        int angle = modifiedSituation.angle;
+        if(move<0){
+          angle+=180;
+          angle=angle%360;
+        };
+        if(angle==0 || angle ==360 ){
+            y = move;
+            x = 0;
+        }else if(angle == 90){
+            x = move;
+            y = 0;
+        }
+        else if(angle==180){
+            y = -move;
+            x = 0;
+
+        }else if(angle == 270){
+            x = -move;
+            y = 0;
+        }
+        else if(angle>0 && angle<90){
+            x = (int) (move*Math.sin(angle));
+            y = (int) (move*Math.cos(angle));
+
+        }else if(angle>90 && angle <180){
+            angle=180-angle;
+            x = (int) (move*Math.sin(angle));
+            y = -(int) (move*Math.cos(angle));
+
+
+        }else if(angle>180 && angle<270){
+            angle=270-angle;
+            x = -(int) (move*Math.cos(angle));
+            y = -(int) (move*Math.sin(angle));
+
+        }else if(angle>270 && angle<360){
+            angle=360-angle;
+            x = -(int) (move*Math.sin(angle));
+            y = (int)(move*Math.cos(angle));
+        }
+        modifiedSituation.position.x +=x;
+        modifiedSituation.position.y+=y;
         return modifiedSituation;
     }
 

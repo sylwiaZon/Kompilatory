ee59081cebca6e04b4d8d11899ba2e8e88fc61ad
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-06-03 20:56:10 +0200

    back/forward fixed

--- LogoCompiler/src/Logo/Compiler/LogoMoveCommand.java
+++ LogoCompiler/src/Logo/Compiler/LogoMoveCommand.java
@@ -1,10 +1,13 @@
 package Logo.Compiler;
 
+
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+
 
 public class LogoMoveCommand implements LogoCommandInterface {
     public int move;
@@ -26,6 +29,7 @@ public class LogoMoveCommand implements LogoCommandInterface {
           angle+=180;
           angle=angle%360;
         };
+        move = Math.abs(move);
         if(angle==0 || angle ==360 ){
             y = move;
             x = 0;
@@ -61,7 +65,7 @@ public class LogoMoveCommand implements LogoCommandInterface {
             x = -(int) (move*Math.sin(angle));
             y = (int)(move*Math.cos(angle));
         }
-        modifiedSituation.position.x +=x;
+        modifiedSituation.position.x+=x;
         modifiedSituation.position.y+=y;
         return modifiedSituation;
     }
@@ -72,6 +76,7 @@ public class LogoMoveCommand implements LogoCommandInterface {
         gc.setStroke(Color.GREEN);
         gc.setLineWidth(5);
         gc.strokeLine(curentSituation.position.x, curentSituation.position.y, modifiedSituation.position.x, modifiedSituation.position.y);
+       
         return canvas;
     }
 }
